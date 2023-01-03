61ea93ac889d0a7f673270f20a2672237409bedc
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-03 12:05:45 -0800

    task 6 , combinations pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for d1 in range(0, 10):
     for d2 in range(d1 + 1, 10):
         if d1 == 8 and d2 == 9:
-            print("{}{}".format(d1,d2))
+            print("{}{}".format(d1, d2))
         else:
-            print("{}{}".format(d1,d2), end=", ")
+            print("{}{}".format(d1, d2), end=", ")
