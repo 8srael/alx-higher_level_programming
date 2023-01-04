49c7360e55833fd1d99e6626f28fde0996b84cc2
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-03 22:02:50 -0800

    task 14 , alphabet in reverse order alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for letter in range(122, 96, -1):
-    if letter % 2 == 0:
-        print("{}".format(chr(letter)), end='')
+for j in range(ord('Z'), ord('A') - 1, -1):
+    if j % 2 != 0:
+        print("{}".format(chr(j)), end='')
     else:
-        print("{}".format(chr(letter - 32)), end='')
+        print("{}".format(chr(j + 32)), end='')
