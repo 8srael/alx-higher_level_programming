6000a06dd6654198dead46e5a1154065465bed60
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-05 03:12:52 -0800

    task 2 , use sys module to print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+
+args_number = len(sys.argv) - 1
+
+print("{} arguments".format(args_number), end='')
+
+if args_number == 0:
+    print('.')
+else:
+    print(':')
+    for i in range(1, args_number + 1):
+        print("{}: {}".format(i, sys.argv[i]))
