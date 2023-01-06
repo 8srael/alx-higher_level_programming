8042f7ac81c77a874125787e2ffa7d554952c3c1
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-06 05:03:45 -0800

    task 4, replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        for i in range(0, len(my_list)):
+            new_list.append(my_list[i])
+        new_list[idx] = element
+        return new_list
