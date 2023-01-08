d01577616ed356171db08e61fcf83b4ce3b54947
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-08 13:30:05 -0800

    task 4 updated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     new_list = []
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         for i in range(0, len(my_list)):
