78ee291d39801bce6b56535caacce2e4a9be466e
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-09 14:31:22 -0800

    task 9 , biggest number in a list, file updated

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
+
     maximum = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] > maximum:
