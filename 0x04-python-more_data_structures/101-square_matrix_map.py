517cde710b3723090e54984271569b1d01edfc1a
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-11 15:48:25 -0800

    task 14, squared by using map updated

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map((lambda z: z * z), number)) for number in matrix]
+    return list(map((lambda row: list(map((lambda z: z * z), row))), matrix))
