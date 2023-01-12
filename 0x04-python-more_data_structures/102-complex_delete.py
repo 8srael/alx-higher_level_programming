abd1f4501780837d6d6072d9fe1d126e5c3d4800
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-11 16:03:22 -0800

    task 12 , complex delete upadated

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value:
-        for k, v in list(a_dictionary.items()):
-            if v == value:
-                del a_dictionary[key]
-        return a_dictionary
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+    return a_dictionary
