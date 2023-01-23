9ee185247fd723fbe4bd935e5ecbaed00d6e8441
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-23 15:49:36 -0800

    task 2 , try catch , safe print list integers updated

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     for j in range(x):
@@ -8,6 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             i += 1
         except(ValueError, TypeError):
             continue
-        print()
-
-        return i
+    print()
+    return i
