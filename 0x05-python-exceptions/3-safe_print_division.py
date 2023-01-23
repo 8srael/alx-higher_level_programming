00102338e6e990f866c4ad1f70fe6203fa729499
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-23 15:58:13 -0800

    task 3, integers division with debug updated

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         division = a / b
     except ZeroDivisionError:
-        div = None
+        division = None
     finally:
         print("Inside result: {}".format(division))
 
