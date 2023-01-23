33df7e9280c0785813742e3b58cead203b5b8dc4
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-23 09:24:07 -0800

    task 0, using of try catch, safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for y in range(x):
+        try:
+            print(my_list[y], end="")
+            i += 1
+        except IndexError:
+            break
+
+    print()
+    return i
