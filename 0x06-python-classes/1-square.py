a04cc4f500df8f046918939147d5fcc0dd00aab8
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-24 15:53:26 -0800

    task 1 , instantiate a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Representation of a square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
+            size(int): The size of a new square
+        """
+
+        self.__size = size
