ed1882c994cec1b8bb7a8d28b05a5e80aebb2655
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-24 16:19:38 -0800

    task 3, area of a square, pycodestyle updated

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,11 +13,13 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif:
+        elif size < 0:
             raise ValueError('size must be >= 0')
 
         self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
+        """Returns:
+            The current area of the square.
+        """
         return self.__size * self.__size
