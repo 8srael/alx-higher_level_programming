41a7454a6388b18475a697d3cf8c11afc796a9a7
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-11 19:16:19 -0700

    task 10 updated

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 function factoriel (x) {
- if (x == 0 || Number.isNaN(x)) {
-   return 1;
+  if (x == 0 || Number.isNaN(x)) {
+    return 1;
  } else {
-   return x * factoriel (x - 1);
+    return x * factoriel (x - 1);
  }
-}
 
 const argNum = parseInt(process.argv[2]);
 console.log(factoriel(argNum));
