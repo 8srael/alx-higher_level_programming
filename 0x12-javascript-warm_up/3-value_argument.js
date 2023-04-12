4d2bceae8d243dc8c2d3899504f9a818de079b02
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-11 19:02:39 -0700

    task 3 updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 if (args[0]) {
-  console.log(args[0])
+  console.log(args[0]);
 } else {
-  console.log('No argument')
+  console.log('No argument');
 }
