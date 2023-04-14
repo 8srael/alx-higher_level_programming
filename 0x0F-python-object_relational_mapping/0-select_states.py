9d4de7d0e29d51985e02c5f3235f65bf942a76b6
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-14 13:37:09 -0700

    task 0 , modify sql code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     dbrows = cur.fetchall()
 
     for row in dbrows:
