a45d67e67a852dea732ce335ed24e843a73f2f36
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-14 16:20:43 -0700

    task 1 , first way

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,15 +10,12 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset='utf8')
     cur = db.cursor()
 
-    query = """SELECT * FROM states
-                WHERE states.name LIKE 'N%'
-                    ORDER BY states.id ASC"""
-
-    cur.execute(query)
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     dbrows = cur.fetchall()
 
     for row in dbrows:
-        print(row)
+        if row[1].startswith('N'):
+            print(row)
 
     cur.close()
     db.close()
