2df17b7cfe5adfafedd3b80f9b4aa7f714eaaac7
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-14 19:12:56 -0700

    task 8 , the first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Start link class to table in database"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    firstObj = session.query(State).first()
+    print("{}: {}".format(firstObj.id, firstObj.name))
+
+    session.close()
