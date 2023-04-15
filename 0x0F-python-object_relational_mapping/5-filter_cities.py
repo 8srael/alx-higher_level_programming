2d676756659d08e752e7e08a24bdd6a71e5417a3
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-14 17:57:17 -0700

    task 5 , all cities of a state , change print logic

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,10 +10,12 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset='utf8')
     cur = db.cursor()
 
-    cur.execute("""SELECT cities.name FROM cities
-                    JOIN states ON cities.state_id = states.id
-                        WHERE states.name = %s
-                             ORDER BY cities.id ASC""", (argv[4], ))
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
 
     print(", ".join(map(lambda a: a[0],cur.fetchall())))
 
