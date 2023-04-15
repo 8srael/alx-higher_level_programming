38f03496e16fbd3e059f13f5267678848fd6674d
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-14 17:12:59 -0700

    task 4 , change sql query using join , add space

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
-    query = """SELECT cities.id, cities.name,states.name FROM cities
+    query = """SELECT cities.id, cities.name, states.name FROM cities
                     JOIN states ON cities.state_id = states.id
                         ORDER BY cities.id ASC"""
     cur.execute(query)
