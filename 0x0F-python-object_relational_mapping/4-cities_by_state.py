31831efc21fab6f1d4ebfaaef683daaf16708490
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-14 17:11:53 -0700

    task 4 , change sql query using join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,10 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT id, name FROM cities ORDER BY cities.id ASC")
+    query = """SELECT cities.id, cities.name,states.name FROM cities
+                    JOIN states ON cities.state_id = states.id
+                        ORDER BY cities.id ASC"""
+    cur.execute(query)
     dbrows = cur.fetchall()
 
     for row in dbrows:
