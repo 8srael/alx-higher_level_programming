4e50ea06dbee4955c291f50fd37d2ba55f470c5c
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-14 17:00:04 -0700

    task 4 , take place to cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Lists states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    dbrows = cur.fetchall()
+
+    for row in dbrows:
+        print(row)
+
+    cur.close()
+    db.close()
