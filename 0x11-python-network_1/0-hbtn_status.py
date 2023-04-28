dba1cbac6e09e99df07b4f18324b26e707e91dca
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-28 01:14:33 -0700

    Task 0, what's my status ?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
-import urllib.request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+    import urllib.request
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
