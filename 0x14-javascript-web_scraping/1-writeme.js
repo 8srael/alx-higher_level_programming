1635a45eb75f631a63a5faafd9f7048519cabd93
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-05-23 17:25:04 -0700

    Task 1, Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
